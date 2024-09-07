931521b62f968f8c3138bd6c6e6ac423a9a50f3f
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-09-07 13:22:21 +0300

    Update B. Минимальное число.py

--- Разминка/B. Минимальное число.py
+++ Разминка/B. Минимальное число.py
@@ -1,1 +1,12 @@
+x = input()
+y = []
+for i in x:
+    y.append(int(i))
 
+y.sort()
+if sum(y) != 0 and y.count(0) != 0:
+    y[0] = y[y.count(0)]
+    y[y.count(0) + 1] = 0
+
+for i in y:
+    print(i, end="")

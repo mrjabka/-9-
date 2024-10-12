ca0a62449ddf78b6898849ba997dd6fa72774eae
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-10-12 12:49:16 +0300

    Update С. Двоичный поиск.py

--- Бинарный поиск/С. Двоичный поиск.py
+++ Бинарный поиск/С. Двоичный поиск.py
@@ -1,1 +1,27 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+
+
+n, k = map(int, input().split())
+sorted_array = list(map(int, input().split()))
+query_array = list(map(int, input().split()))
+
+results = []
+for number in query_array:
+    if binary_search(sorted_array, number):
+        results.append("YES")
+    else:
+        results.append("NO")
+
+for result in results:
+    print(result)
 

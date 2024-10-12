609b944fd36e9c2354cbaaa913a2a3a81866749f
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-10-12 10:10:29 +0300

    Update H. Пересадки.py

--- Разминка/H. Пересадки.py
+++ Разминка/H. Пересадки.py
@@ -1,5 +1,4 @@
 def count_common_stops(a1, b1, a2, b2):
-    # Определяем диапазон остановок для каждого автобуса
     range1 = set(range(min(a1, b1), max(a1, b1) + 1))
     range2 = set(range(min(a2, b2), max(a2, b2) + 1))
 

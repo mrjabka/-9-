f9dd1d7d58e8d1ed1e9a4729d2587ac6d1c9a3d1
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-11-16 13:37:51 +0300

    Update A. Массивы.py

--- Бинарный поиск-2/A. Массивы.py
+++ Бинарный поиск-2/A. Массивы.py
@@ -1,1 +1,18 @@
+i = int(input())
+list_i = list(map(int, input().split()))
 
+j = int(input())
+list_j = list(map(int, input().split()))
+
+count_table = {}
+for num in list_i:
+    if num not in count_table:
+        count_table[num] = 0
+    count_table[num] += 1
+
+output = []
+
+for num in list_j:
+    output.append(count_table.get(num, 0))
+
+print(*output)

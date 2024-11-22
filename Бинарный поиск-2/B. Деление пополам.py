8bec915691b0824c40ca089f4dcf16ea731cb303
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-11-22 22:27:55 +0300

    Update B. Деление пополам.py

--- Бинарный поиск-2/B. Деление пополам.py
+++ Бинарный поиск-2/B. Деление пополам.py
@@ -1,1 +1,20 @@
+a = float(input())
+n = int(input())
+val = 0
 
+if __name__ == '__main__':
+    if a < 1:
+        r = 1
+    else:
+        r = a
+
+    m = r / 2
+
+    for i in range(200):
+        if m ** n >= a:
+            r = m
+        else:
+            val = m
+        m = (val + r) / 2
+
+    print(r)

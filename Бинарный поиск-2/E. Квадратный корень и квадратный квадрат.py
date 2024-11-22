35c8c245c3111b142c1e32c63702df7fb69b54b6
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-11-22 23:27:11 +0300

    Update E. Квадратный корень и квадратный квадрат.py

--- Бинарный поиск-2/E. Квадратный корень и квадратный квадрат.py
+++ Бинарный поиск-2/E. Квадратный корень и квадратный квадрат.py
@@ -1,1 +1,21 @@
+c = float(input())
+eps = 1.0e-7
 
+
+def solve(f, a, b):
+
+    while b-a > eps:
+        mid = (a + b) / 2
+        y = f(mid)
+
+        if y > c:
+            b = mid
+        else:
+            a = mid
+
+    return a
+
+
+if __name__ == '__main__':
+    result = solve(lambda x: x**2+x**0.5, 0, c)
+    print('{:.7f}'.format(result))

529994f1f2cd665c610438bc560f99d4526e490b
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-11-23 13:12:30 +0300

    Update F. Провода.py

--- Бинарный поиск-2/F. Провода.py
+++ Бинарный поиск-2/F. Провода.py
@@ -1,1 +1,33 @@
+import numpy as np
 
+n, k = map(int, input().split())
+l = [int(input()) for i in range(n)]
+
+
+def cut(a, b, length):
+
+    length = np.array(length)
+
+    left = 1
+    right = max(length)
+    result = 0
+
+    while left <= right:
+
+        mid = (left + right) // 2
+        count = 0
+
+        for l in length:
+            count += l // mid
+
+        if count >= b:
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
+
+
+if __name__ == '__main__':
+    print(cut(n, k, l))

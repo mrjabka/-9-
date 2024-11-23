81340d2f0f4a4801bb846c40a1d9dabe01e2045e
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-11-23 12:56:57 +0300

    Update C. Арксинус.py

--- Бинарный поиск-2/C. Арксинус.py
+++ Бинарный поиск-2/C. Арксинус.py
@@ -1,1 +1,23 @@
+from math import sin, pi
 
+a = float(input())
+x = 0
+
+left = - pi / 2
+right = pi / 2
+
+eps = 1.0e-7
+
+
+if __name__ == '__main__':
+
+    while right - left > eps:
+
+        mid = (left + right) / 2
+
+        if sin(mid) < a:
+            left = mid
+        else:
+            right = mid
+
+    print(left)

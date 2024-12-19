e71a7c208f7294db59556884f303418282c55802
mrjabka <157302347+mrjabka@users.noreply.github.com>
2024-12-19 15:59:08 +0300

    Update D. Сортировка вставками.py

--- Сортировка массивов/D. Сортировка вставками.py
+++ Сортировка массивов/D. Сортировка вставками.py
@@ -1,18 +1,1 @@
-length = int(input())
-mass = list(map(int, input().split()[:length]))
 
-
-def insert_sort(massive):
-    output = []
-
-    for i in range(len(massive)):
-        output.append(min(massive))
-        massive.remove(min(massive))
-
-    print(*output)
-
-    return None
-
-
-if __name__ == '__main__':
-    insert_sort(mass)

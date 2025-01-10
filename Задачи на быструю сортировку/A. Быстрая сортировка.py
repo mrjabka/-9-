3b2b45a49148087a3bc4b6fd7142355b49b44d4d
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-01-10 19:27:13 +0300

    Update A. Быстрая сортировка.py

--- Задачи на быструю сортировку/A. Быстрая сортировка.py
+++ Задачи на быструю сортировку/A. Быстрая сортировка.py
@@ -1,1 +1,20 @@
+size = int(input())
+massive = list(map(int, input().split()[:size]))
+
+
+def quick_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+if __name__ == '__main__':
+    print(*quick_sort(massive))
 

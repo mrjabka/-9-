0743ca4078c8716dae73ffca3694323d3fe870d7
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-01-10 19:57:08 +0300

    Update B. Сортировка (D).py

--- Задачи на быструю сортировку/B. Сортировка (D).py
+++ Задачи на быструю сортировку/B. Сортировка (D).py
@@ -1,1 +1,36 @@
+n = int(input())
+numbers = [int(input()) for i in range(n)]
+
+
+def quick_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+def sort(k, arr):
+
+    uniques = list(dict.fromkeys(arr))
+
+    if k % 2 == 0:
+        output = uniques
+        for val in output:
+            print(val)
+    else:
+        output = uniques[::-1]
+        for val in output:
+            print(val)
+
+    return None
+
+
+if __name__ == '__main__':
+    sort(n, numbers)
 

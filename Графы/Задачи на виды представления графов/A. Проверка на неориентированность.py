0611758d1371fde46a7dc895ab6dae8e5a96773f
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 13:48:00 +0300

    Update A. Проверка на неориентированность.py

--- Графы/Задачи на виды представления графов/A. Проверка на неориентированность.py
+++ Графы/Задачи на виды представления графов/A. Проверка на неориентированность.py
@@ -1,1 +1,18 @@
+def is_valid_adjacency_matrix(matrix, n):
 
+    for i in range(n):
+        if matrix[i][i] != 0:
+            return "NO"
+
+        for j in range(n):
+            if matrix[i][j] != matrix[j][i]:
+                return "NO"
+
+    return "YES"
+
+
+if __name__ == '__main__':
+    k = int(input())
+    m = [list(map(int, input().split())) for _ in range(k)]
+
+    print(is_valid_adjacency_matrix(m, k))

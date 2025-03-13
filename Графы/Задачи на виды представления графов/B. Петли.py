dcfd7a764715e84f4a4258f9f03e8ed47dae57e5
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 13:58:04 +0300

    Update B. Петли.py

--- Графы/Задачи на виды представления графов/B. Петли.py
+++ Графы/Задачи на виды представления графов/B. Петли.py
@@ -1,1 +1,14 @@
+def contains_loops(matrix, n):
 
+    for i in range(n):
+        if matrix[i][i] == 1:
+            return "YES"
+
+    return "NO"
+
+
+if __name__ == '__main__':
+    k = int(input())
+    m = [list(map(int, input().split())) for _ in range(k)]
+
+    print(contains_loops(m, k))

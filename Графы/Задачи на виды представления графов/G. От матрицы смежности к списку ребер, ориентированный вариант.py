bd84e765d1b08747f42012c6c15fa0e69fc3a9a0
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 14:31:33 +0300

    Update G. От матрицы смежности к списку ребер, ориентированный вариант.py

--- Графы/Задачи на виды представления графов/G. От матрицы смежности к списку ребер, ориентированный вариант.py
+++ Графы/Задачи на виды представления графов/G. От матрицы смежности к списку ребер, ориентированный вариант.py
@@ -1,1 +1,17 @@
+def get_edge_list(matrix, n):
+    edge_list = []
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == 1:
+                edge_list.append((i + 1, j + 1))
+    return edge_list
 
+
+if __name__ == '__main__':
+    k = int(input())
+    m = [list(map(int, input().split())) for _ in range(k)]
+
+    edges = get_edge_list(m, k)
+
+    for edge in edges:
+        print(edge[0], edge[1])

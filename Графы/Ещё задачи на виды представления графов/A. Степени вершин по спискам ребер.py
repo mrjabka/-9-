2875ee95d17dc024a87fbb7e75e95d49853f4072
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 16:42:27 +0300

    Update A. Степени вершин по спискам ребер.py

--- Графы/Ещё задачи на виды представления графов/A. Степени вершин по спискам ребер.py
+++ Графы/Ещё задачи на виды представления графов/A. Степени вершин по спискам ребер.py
@@ -1,1 +1,22 @@
+def calculate_degrees(n, edges):
 
+    degrees = [0] * n
+
+    for u, v in edges:
+        degrees[u - 1] += 1
+        degrees[v - 1] += 1
+
+    return degrees
+
+
+# Ввод данных
+n, m = map(int, input().split())
+
+edges = []
+for _ in range(m):
+    u, v = map(int, input().split())
+    edges.append((u, v))
+
+degrees = calculate_degrees(n, edges)
+
+print('\n'.join(map(str, degrees)))

db0a6098d4c77277daf1a29d114b42363a67d79b
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 14:16:28 +0300

    Update D.  Подсчет количества ребер ориентированного графа.py

--- Графы/Задачи на виды представления графов/D.  Подсчет количества ребер ориентированного графа.py
+++ Графы/Задачи на виды представления графов/D.  Подсчет количества ребер ориентированного графа.py
@@ -1,1 +1,14 @@
+def count_edges(matrix, n):
+    total_edges = 0
+    for i in range(n):
+        for j in range(n):
+            total_edges += matrix[i][j]
 
+    return total_edges
+
+
+if __name__ == '__main__':
+    k = int(input())
+    m = [list(map(int, input().split())) for _ in range(k)]
+
+    print(count_edges(m, k))

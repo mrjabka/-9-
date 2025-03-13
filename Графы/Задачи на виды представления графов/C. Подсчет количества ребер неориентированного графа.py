a59dba2fe47e0c6aa587ee74b4614bb8d9664de1
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-13 14:07:33 +0300

    Update C. Подсчет количества ребер неориентированного графа.py

--- Графы/Задачи на виды представления графов/C. Подсчет количества ребер неориентированного графа.py
+++ Графы/Задачи на виды представления графов/C. Подсчет количества ребер неориентированного графа.py
@@ -1,1 +1,15 @@
+def count_edges(matrix, n):
+    total_edges = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            total_edges += matrix[i][j]
 
+    return total_edges
+
+
+if __name__ == '__main__':
+    k = int(input())
+    m = [list(map(int, input().split())) for _ in range(k)]
+
+    print(count_edges(m, k))
+  

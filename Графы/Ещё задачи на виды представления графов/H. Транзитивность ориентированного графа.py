47d2ee2d405e99b53a7f9e948e8f0762e1ba1842
mrjabka <157302347+mrjabka@users.noreply.github.com>
2025-03-14 14:41:25 +0300

    Update H. Транзитивность ориентированного графа.py

--- Графы/Ещё задачи на виды представления графов/H. Транзитивность ориентированного графа.py
+++ Графы/Ещё задачи на виды представления графов/H. Транзитивность ориентированного графа.py
@@ -1,1 +1,20 @@
+def is_transitive_graph(n, adjacency_matrix):
 
+    for u in range(n):
+        for v in range(n):
+            if u != v and adjacency_matrix[u][v] == 1:
+                for w in range(n):
+                    if w != u and w != v:
+                        # Если есть (u -> v) и (v -> w), но нет (u -> w), то граф не транзитивен
+                        if adjacency_matrix[v][w] == 1 and adjacency_matrix[u][w] == 0:
+                            return "NO"
+    return "YES"
+
+
+if __name__ == '__main__':
+    n = int(input())
+    adjacency_matrix = [list(map(int, input().split())) for _ in range(n)]
+
+    result = is_transitive_graph(n, adjacency_matrix)
+
+    print(result)
